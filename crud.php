ca10287355c4e5ae4746ca16993b9c609de73e07
Jessica Wong <wongjessica124@gmail.com>
2021-04-01 18:32:21 +0700

    Menambahkan condition untuk delete @line-4. Menambahkan condition jika berhasil atau tidak berhasil menghapus @line-38. Menambahkan fungsi untuk update @line-73. Menambahkan fungsi untuk mengambil minat berdasarkan id mahasiswa @line-95

--- crud.php
+++ crud.php
@@ -1,6 +1,12 @@
 <?php
 require_once("connect_db.php");
 
+if(isset($_GET['id']) && isset($_GET['action'])) {
+  if($_GET['action'] == 'delete') {
+    deleteWhere('data_mahasiswa','id',$_GET['id']);
+  }
+}
+
 function getAllData($tbl)
 {
   global $conn;
@@ -27,9 +33,20 @@ function deleteWhere($tbl, $field, $condition)
 {
   global $conn;
   $query = "DELETE FROM $tbl WHERE $field = $condition";
-  $result = mysqli_query($conn, $query);
+  mysqli_query($conn, $query);
 
-  return $result;
+  if(mysqli_affected_rows($conn) > 0) {
+    echo "<script>
+            alert('Data berhasil dihapus!');
+            document.location.href = 'tabel.php';
+          </script>";
+  } else {
+    echo "<script>
+            alert('Data gagal dihapus!');
+            document.location.href = 'tabel.php';
+          </script>";
+
+  }
 }
 
 function addData($data)
@@ -45,10 +62,40 @@ function addData($data)
   $biografi = htmlspecialchars($data['student_bio']);
   $jurusan = htmlspecialchars($data['student_courses']);
   $minat = implode(",", $data['user_interest']);
-
+  
   $query = "INSERT INTO data_mahasiswa VALUE ('$id','$nim','$nama','$jenis_kelamin','$email','$usia','$biografi','$jurusan','$minat',DEFAULT,DEFAULT)";
+  mysqli_query($conn, $query);
+  $result = mysqli_affected_rows($conn);
+  
+  return $result;
+}
+
+function updateDataMhs($data) {
+  global $conn;
+
+  $id = $data['id'];
+  $nama = ucwords(htmlspecialchars($data['nama_mhs']));
+  $nim = htmlspecialchars($data['nim_mhs']);
+  $jenis_kelamin = htmlspecialchars($data['jenis_kelamin']);
+  $email = htmlspecialchars($data['email_mhs']);
+  $usia = $data['usia_mhs'];
+  $biografi = htmlspecialchars($data['student_bio']);
+  $jurusan = htmlspecialchars($data['student_courses']);
+  $minat = implode(",", $data['user_interest']);
+
+  $query = "UPDATE data_mahasiswa SET 
+            nama = '$nama', nim = '$nim', jenis_kelamin = '$jenis_kelamin',email = '$email',usia = '$usia', biografi = '$biografi', kode_jurusan = '$jurusan', id_minat = '$minat', updated_at = DEFAULT  
+            WHERE id = $id";
   mysqli_query($conn, $query);
   $result = mysqli_affected_rows($conn);
 
   return $result;
+}
+
+function getMinat($id) {
+  global $conn;
+  $query = "SELECT minat from minat WHERE id = $id";
+  $result = mysqli_query($conn,$query);
+  $minat = mysqli_fetch_assoc($result);
+  return $minat['minat'];
 }
